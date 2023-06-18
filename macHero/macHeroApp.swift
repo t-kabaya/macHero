4f252c93ff38d783a6792469ed6c9a97f9e7f137
Kabaya Taiki <taikikabaya@gmail.com>
2023-06-18 13:26:33 +0900

    super wip

--- macHero/macHeroApp.swift
+++ macHero/macHeroApp.swift
@@ -10,15 +10,26 @@ import SwiftUI
 @main
 struct macHeroApp: App {
     @State var currentNumber: String = "1"
+    
+    func playAlerm() -> Void {
+        NSSound(named: "alerm")?.play()
+    }
 
     var body: some Scene {
         MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
             // 3
             Button("One") {
                 currentNumber = "1"
+                playAlerm()
             }
             Button("Two") {
                 currentNumber = "2"
+                let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
+                let accessEnabled =     (options)
+
+                if !accessEnabled {
+                    print("Access Not Enabled")
+                }
             }
             Button("Three") {
                 currentNumber = "3"
